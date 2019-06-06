25ec1ae985cc98f6dd1f495318c524b4bbf74b14
tatsianabaida <tatsianabaida@bk.ru>
2019-06-07 02:15:24 +0300

    :: CheckPoint 1 ::

---
+++ web/src/main/java/by/itacademy/web/util/JspPath.java
@@ -0,0 +1,14 @@
+package by.itacademy.web.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class JspPath {
+
+    private static final String FORMAT = "/WEB-INF/jsp/%s.jsp";
+
+    public static String get(String jspName) {
+        return String.format(FORMAT, jspName);
+    }
+}
